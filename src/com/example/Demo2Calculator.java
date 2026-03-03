ad8241fa49c5e203f51f74624f12dfaab36ae9eb
Luyic <Luyicheng--20060922@outlook.com>
2026-03-03 12:05:25 +0800

    更新

--- src/com/example/Demo2Calculator.java
+++ src/com/example/Demo2Calculator.java
@@ -1,4 +1,32 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Demo2Calculator {
+      public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          double num1 = sc.nextDouble();
+          char operator = sc.next().charAt(0);
+          double num2 = sc.nextDouble();
+          if(operator == '+'){
+              System.out.println(num1 + num2);
+
+          }
+          else if(operator == '-'){
+              System.out.println(num1 - num2);
+          }
+          else if(operator == '*'){
+              System.out.println(num1 * num2);
+          }
+          else if(operator == '/'){
+              if(num2 == 0){
+                  System.out.println("Division by zero");
+
+              }
+              else{
+                  System.out.println(num1 / num2);
+              }
+          }
+          sc.close();
+      }
 }
